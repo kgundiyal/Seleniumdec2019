b1e04e8d5fd0fdb40b0ff9b3685ae030ccb0beff
kgundiyal <kgundiyal@MCD-PCD-0063.mcd.principlemcd.com>
2019-12-13 16:03:47 -0600

    Updated code to add chrome

--- day1/src/day1package/day1class.java
+++ day1/src/day1package/day1class.java
@@ -1,10 +1,18 @@
 package day1package;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class day1class
 {
 	public static void main(String args[])
 	{
-
+		System.setProperty("webdriver.chrome.driver","C://Users//kgundiyal//Documents//Selenium//chromedriver.exe");
+		WebDriver driver1=new ChromeDriver();
+		driver1.get("http://google.com");
+		String pgtitle=driver1.getTitle();
 		System.out.println("hello world! hello world!");
+		System.out.println(pgtitle);
+		driver1.close();
 	}
 }
